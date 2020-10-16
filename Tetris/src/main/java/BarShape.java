98ec0c7fcf4b28fcf84119238e01e0593c136834
Aziz <aziz98366@gmail.com>
2020-10-16 04:33:49 -0700

    Rotate is complete. Does not rotate if piece is not allowed to rotate

--- Tetris/src/main/java/BarShape.java
+++ Tetris/src/main/java/BarShape.java
@@ -15,21 +15,6 @@ public class BarShape extends AbstractPiece {
         square[3] = new Square(g, r, c + 2, Color.cyan, true);
     }
 
-    /** This rotates the Piece */
-    public void rotate() {
-        for (int i = 0; i < PIECE_COUNT; i++) {
-
-            int py = square[1].getRow();
-            int px = square[1].getCol();
-            int y1 = square[i].getRow();
-            int x1 = square[i].getCol();
-            int x2 = px + py - y1;
-            int y2 = x1 + py - px;
-            square[i].setCol(x2);
-            square[i].setRow(y2);
-
-        }
-
-    }
+    
 
 }
